35a00247c71c73a1c36db4d05d80fb354b3dd29f
Martin Procházka <prochazka@cngroup.dk>
2021-12-05 09:15:40 +0100

    Main program refactoring

--- 2021/Application/Program.cs
+++ 2021/Application/Program.cs
@@ -8,24 +8,31 @@ using Business.Day6;
 
 namespace Application
 {
+    /// <summary>
+    /// https://adventofcode.com/2021
+    /// </summary>
     internal static class Program
     {
+        private static readonly IService[] Services = {
+            new Business.Day1.Service(new SonarData()),
+            new Business.Day2.Service(new NavigationData()),
+            new Business.Day3.Service(new DiagnosticsData()),
+            new Business.Day4.Service(new BingoData()),
+            new Business.Day5.Service(new FloorData()),
+            new Business.Day6.Service(new Day6Data()),
+        };
+
         private static void Main(string[] args)
         {
-            var services = new IService[]
-            {
-                new Business.Day1.Service(new SonarData()),
-                new Business.Day2.Service(new NavigationData()),
-                new Business.Day3.Service(new DiagnosticsData()),
-                new Business.Day4.Service(new BingoData()),
-                new Business.Day5.Service(new FloorData()),
-                new Business.Day6.Service(new Day6Data()),
-            };
+            RunServices();
+        }
 
-            for (var i = 0; i < services.Length; i++)
+        private static void RunServices()
+        {
+            for (var i = 0; i < Services.Length; i++)
             {
-                Log.WriteResult(i + 1, 1, services[i].RunTask1());
-                Log.WriteResult(i + 1, 2, services[i].RunTask2());
+                Log.WriteResult(i + 1, 1, Services[i].RunTask1());
+                Log.WriteResult(i + 1, 2, Services[i].RunTask2());
             }
         }
     }
