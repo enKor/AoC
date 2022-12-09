fb0c84059ca9caec307f605aa07ef0d1134b02e5
Martin Procházka <prochazka@cngroup.dk>
2022-12-09 22:08:32 +0100

    22/

--- 2022/aoc/Day8.cs
+++ 2022/aoc/Day8.cs
@@ -5,97 +5,185 @@ namespace aoc;
 [TestFixture]
 public class Day8
 {
-    private int[][] _forrest;
+    private List<string> input;
 
     [SetUp]
     public void SetUp()
     {
-        var data = File.ReadAllLines("Day8.txt");
-
-        _forrest = new int[data.Length][];
-        for (int y = 0; y < data.Length; y++)
-        {
-            _forrest[y] = new int[data[y].Length];
-            for (int x = 0; x < data[y].Length; x++)
-            {
-                _forrest[y][x] = Convert.ToInt32(data[y][x].ToString());
-            }
-        }
+        var inputFile= File.ReadAllLines("Day8.txt");
+        input = new List<string>(inputFile);
     }
 
     [Test]
     public void Part1()
     {
-        var trees = new List<Tree>();
-
-        for (int y = 1; y < _forrest.Length - 1; y++)
+        var grid = new List<List<int>>();
+        foreach (var line in input)
         {
-            var highest = 0;
+            var row = new List<int>();
 
-            for (int x = 1; x < _forrest[y].Length - 1; x++)
+            foreach (var tree in line)
             {
-                if (x == 1) highest = _forrest[y][x - 1];
-
-                var current = _forrest[y][x];
-                if (highest < current)
-                {
-                    trees.Add(new Tree(y, x));
-                    highest = current;
-                }
+                row.Add(Convert.ToInt32(tree));
             }
-            for (int x = _forrest[y].Length - 2; x > 0; x--)
-            {
-                if (x == 1) highest = _forrest[y][x + 1];
 
-                var current = _forrest[y][x];
-                if (highest < current)
+            grid.Add(row);
+        }
+
+        int treesVisible = 0;
+
+        treesVisible += (grid.Count * 2) + (grid[0].Count * 2) - 4;
+
+        for (int row = 1; row < grid.Count - 1; row++)
+        {
+            for (int col = 1; col < grid[row].Count - 1; col++)
+            {
+                int currentHeight = grid[row][col];
+
+                bool isVisible = true;
+
+                //left check
+                for (int leftCol = col - 1; leftCol >= 0; leftCol--)
                 {
-                    trees.Add(new Tree(y, x));
-                    highest = current;
+                    if (grid[row][leftCol] >= currentHeight)
+                    {
+                        isVisible = false;
+                        break;
+                    }
+                }
+                if (isVisible)
+                {
+                    treesVisible++;
+                    continue;
+                }
+                isVisible = true;
+                //right check
+                for (int rightCol = col + 1; rightCol < grid[row].Count; rightCol++)
+                {
+                    if (grid[row][rightCol] >= currentHeight)
+                    {
+                        isVisible = false;
+                        break;
+                    }
+                }
+                if (isVisible)
+                {
+                    treesVisible++;
+                    continue;
+                }
+                isVisible = true;
+                //up check
+                for (int upRow = row - 1; upRow >= 0; upRow--)
+                {
+                    if (grid[upRow][col] >= currentHeight)
+                    {
+                        isVisible = false;
+                        break;
+                    }
+                }
+                if (isVisible)
+                {
+                    treesVisible++;
+                    continue;
+                }
+                isVisible = true;
+                //down check
+                for (int downRow = row + 1; downRow < grid.Count; downRow++)
+                {
+                    if (grid[downRow][col] >= currentHeight)
+                    {
+                        isVisible = false;
+                        break;
+                    }
+                }
+                if (isVisible)
+                {
+                    treesVisible++;
                 }
             }
         }
-
-        for (int x = 1; x < _forrest[0].Length - 1; x++)
-        {
-            var highest = 0;
-            for (int y = 1; y < _forrest[x].Length - 1; y++)
-            {
-                if (y == 1) highest = _forrest[y - 1][x];
-
-                var current = _forrest[y][x];
-                if (highest < current)
-                {
-                    trees.Add(new Tree(y, x));
-                    highest = current;
-                }
-            }
-
-            for (int y = _forrest[x].Length - 2; y > 0; y--)
-            {
-                if (y == _forrest[x].Length - 2) highest = _forrest[y + 1][x];
-
-                var current = _forrest[y][x];
-                if (highest < current)
-                {
-                    trees.Add(new Tree(y, x));
-                    highest = current;
-                }
-            }
-        }
-
-
-        var result = trees.Distinct().Count();
-        result += (_forrest.Length - 1) * 2 + (_forrest[0].Length - 1) * 2;
-
-        Assert.That(result, Is.EqualTo(21));
+        Assert.That(treesVisible, Is.EqualTo(21));
     }
 
     [Test]
     public void Part2()
     {
+        var grid = new List<List<int>>();
+        foreach (var line in input)
+        {
+            var row = new List<int>();
 
-        //Assert.That(result, Is.EqualTo(19));
+            foreach (var tree in line)
+            {
+                row.Add(Convert.ToInt32(tree));
+            }
+
+            grid.Add(row);
+        }
+
+        int bestScenicScore = 0;
+
+        for (int row = 0; row < grid.Count; row++)
+        {
+            for (int col = 0; col < grid[row].Count; col++)
+            {
+                int currentHeight = grid[row][col];
+
+                int left = 0;
+                int right = 0;
+                int up = 0;
+                int down = 0;
+
+
+                //left check
+                for (int leftCol = col - 1; leftCol >= 0; leftCol--)
+                {
+                    left++;
+                    if (grid[row][leftCol] >= currentHeight)
+                    {
+                        break;
+                    }
+                }
+
+                //right check
+                for (int rightCol = col + 1; rightCol < grid[row].Count; rightCol++)
+                {
+                    right++;
+                    if (grid[row][rightCol] >= currentHeight)
+                    {
+                        break;
+                    }
+                }
+
+                //up check
+                for (int upRow = row - 1; upRow >= 0; upRow--)
+                {
+                    up++;
+                    if (grid[upRow][col] >= currentHeight)
+                    {
+                        break;
+                    }
+                }
+
+                //down check
+                for (int downRow = row + 1; downRow < grid.Count; downRow++)
+                {
+                    down++;
+                    if (grid[downRow][col] >= currentHeight)
+                    {
+                        break;
+                    }
+                }
+
+                int scenicScore = left * right * up * down;
+
+                if (scenicScore > bestScenicScore)
+                {
+                    bestScenicScore = scenicScore;
+                }
+            }
+        }
+        Assert.That(bestScenicScore, Is.EqualTo(8));
     }
 
     record Tree(int Y, int X);
