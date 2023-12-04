cbf66fa4b48fe46448fc44e3d4df26cb61802dc8
Martin Procházka <prochazka@cngroup.dk>
2023-12-04 22:33:37 +0100

    1B/23

--- 2023/aoc/Day1.cs
+++ 2023/aoc/Day1.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.IO.Pipes;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace aoc;
@@ -7,17 +8,16 @@ namespace aoc;
 [TestFixture]
 public class Day1
 {
-    private string[] codes;
-
     [SetUp]
     public void SetUp()
     {
-        codes = File.ReadAllLines("Day1.txt");
+        
     }
 
     [Test]
     public void Part1()
     {
+        var codes = File.ReadAllLines("Day1A.txt");
         var sum = 0L;
         Span<char> numbers = stackalloc char[2];
         
@@ -52,6 +52,43 @@ public class Day1
     [Test]
     public void Part2()
     {
-       
+        var codes = File.ReadAllLines("Day1B.txt");
+        var sum = 0L;
+        Span<char> numbers = stackalloc char[2];
+
+        var numbersDic = new Dictionary<string, char>
+        {
+            { "0", '0' }, { "1", '1' }, { "2", '2' }, { "3", '3' }, { "4", '4'}, 
+            { "5", '5' }, { "6", '6' }, { "7", '7' }, { "8", '8' }, { "9", '9'},
+            { "one", '1' },
+            { "two", '2' },
+            { "three", '3' },
+            { "four", '4' },
+            { "five", '5' },
+            { "six", '6' },
+            { "seven", '7' },
+            { "eight", '8' },
+            { "nine", '9' },
+        };
+
+        foreach (var c in codes)
+        {
+            var values = numbersDic.Keys
+                .Select(k => new
+                {
+                    Idx = c.IndexOf(k, StringComparison.InvariantCulture),
+                    Key = k
+                })
+                .Where(x => x.Idx >= 0)
+                .OrderBy(x => x.Idx)
+                .ToArray();
+
+            numbers[0] = numbersDic[values[0].Key];
+            numbers[1] = numbersDic[values.Last().Key];
+
+            sum += int.Parse(numbers);
+        }
+
+        Assert.That(sum, Is.EqualTo(281));
     }
 }
